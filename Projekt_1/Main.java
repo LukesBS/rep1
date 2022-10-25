db5fd95ab34050bdee6f87b2359d8760c4788138
Vetter Lukas <Lukas.Vetter@stud.bs7-augsburg.de>
2022-10-25 14:27:27 +0200

    bankautomat

---
+++ Projekt_1/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Passi ist gut im Bankautomatenprogrammieren für bitches!(Er sagt er hat 'nur' an der ps5 rumgespielt ;)");
+    }
+}
