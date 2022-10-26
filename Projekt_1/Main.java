ba2239305b502e9f62bb1a0a0d2760277e9ec1ab
Luke <luaks.vetter@stud.bs7-augsburg.de>
2022-10-26 10:07:01 +0200

    added gitignore

--- Projekt_1/Main.java
+++ Projekt_1/Main.java
@@ -4,6 +4,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Passi ist gut im Bankautomatenprogrammieren für bitches!(Er sagt er hat 'nur' an der ps5 rumgespielt ;)");
+        System.out.println("Hello World!");
     }
 }
