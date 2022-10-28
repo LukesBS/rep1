2aca8541644a6534c995fc6bada8fce85311ecae
Luke <luaks.vetter@stud.bs7-augsburg.de>
2022-10-28 08:21:05 +0200

    a

---
+++ Projekt_1/Website/website2.php
@@ -0,0 +1,66 @@
+<?php
+
+
+function readDb($postData) {
+
+if (isset($postData["minweight"])) {
+
+$dbh = new PDO('mysql:host=localhost:3306;dbname=warpshop',
+
+'root', 'usbw');
+
+$stmt = $dbh->prepare("SELECT Name, ModelNumber, gewicht, groesse
+
+FROM Product WHERE gewicht >= ? ORDER BY gewicht LIMIT 10");
+
+if ($stmt->execute(array($postData["minweight"]))) {
+
+while ($row = $stmt->fetch()) {
+
+echo $row["Name"] ."|" .$row["ModelNumber"]."|" .$row["gewicht"]
+
+."|" .$row["groesse"] ."<br/>";
+
+}
+
+}
+
+$stmt = null;
+
+$dbh = null;
+
+}
+
+} ?>
+
+<!DOCTYPE html>
+
+<html lang="de">
+
+<head>
+
+<meta charset="utf-8" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>PHP und HTML</title>
+
+</head>
+
+<body>
+
+<form action="SeiteB.php" method="post">
+
+Minimalgewicht: <input type="text" name="minweight">
+
+<input type="submit" value="send">
+
+</form>
+
+<br/>
+
+<?php readDb($_POST); ?>
+
+</body>
+
+</html>
