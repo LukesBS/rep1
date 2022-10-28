d186abd3e47c4c849041cfaee05d2db74b4f0e29
Luke <luaks.vetter@stud.bs7-augsburg.de>
2022-10-28 08:27:00 +0200

    formating changes

--- Projekt_1/Website/website2.php
+++ Projekt_1/Website/website2.php
@@ -1,66 +1,41 @@
 <?php
 
-
 function readDb($postData) {
 
-if (isset($postData["minweight"])) {
+    if (isset($postData["minweight"])) {
 
-$dbh = new PDO('mysql:host=localhost:3306;dbname=warpshop',
+        $dbh = new PDO('mysql:host=localhost:3306;dbname=warpshop','root', 'usbw');
+        $stmt = $dbh->prepare("SELECT Name, ModelNumber, gewicht, groesse FROM Product WHERE gewicht >= ? ORDER BY gewicht LIMIT 10");
 
-'root', 'usbw');
+        if ($stmt->execute(array($postData["minweight"]))) {
 
-$stmt = $dbh->prepare("SELECT Name, ModelNumber, gewicht, groesse
+            while ($row = $stmt->fetch()) {
+                echo $row["Name"] ."|" .$row["ModelNumber"]."|" .$row["gewicht"]."|" .$row["groesse"] ."<br/>";
+            }
 
-FROM Product WHERE gewicht >= ? ORDER BY gewicht LIMIT 10");
+        }
 
-if ($stmt->execute(array($postData["minweight"]))) {
+    $stmt = null;
+    $dbh = null;
 
-while ($row = $stmt->fetch()) {
-
-echo $row["Name"] ."|" .$row["ModelNumber"]."|" .$row["gewicht"]
-
-."|" .$row["groesse"] ."<br/>";
-
-}
-
-}
-
-$stmt = null;
-
-$dbh = null;
-
-}
-
-} ?>
+    }
+} 
+?>
 
 <!DOCTYPE html>
-
 <html lang="de">
-
 <head>
-
-<meta charset="utf-8" />
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-<title>PHP und HTML</title>
-
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP und HTML</title>
 </head>
-
 <body>
-
-<form action="SeiteB.php" method="post">
-
-Minimalgewicht: <input type="text" name="minweight">
-
-<input type="submit" value="send">
-
-</form>
-
-<br/>
-
-<?php readDb($_POST); ?>
-
+    <form action="SeiteB.php" method="post">
+        Minimalgewicht: 
+        <input type="text" name="minweight">
+        <input type="submit" value="send">
+    </form>
+    <br/>
+    <?php readDb($_POST); ?>
 </body>
-
 </html>
